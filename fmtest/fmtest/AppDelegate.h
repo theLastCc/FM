8fd31b5b46546a2b9602187635969f694ab2405e
theLastCc <2846366510@qq.com>
2017-04-13 14:23:55 +0800

    1

---
+++ fmtest/fmtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fmtest
+//
+//  Created by 李亮 on 2017/4/13.
+//  Copyright © 2017年 TheLastCc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
