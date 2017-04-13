b22a58df210a7af32f6465ac2f659415b36abac6
theLastCc <2846366510@qq.com>
2017-04-13 14:27:42 +0800

    1

--- fmtest/fmtest/ViewController.m
+++ fmtest/fmtest/ViewController.m
@@ -17,12 +17,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    // Do any additional setup after loading the view, typically from a nib.
-
-    // Do any additional setup after loading the view, typically from a nib.
-
-    // Do any additional setup after loading the view, typically from a nib.
-
 }
 
 
